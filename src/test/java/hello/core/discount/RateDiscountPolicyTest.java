e8e384479c0419fff1d0b46071d6c6926104ec5a
kcsc2217 <k12002@nate.com>
2023-12-25 17:39:14 +0900

    DI를 적용하여 코드 리팩토링

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RateDiscountPolicyTest {
@@ -23,13 +24,22 @@ class RateDiscountPolicyTest {
 
         //then
 
-        org.assertj.core.api.Assertions.assertThat(discount).isEqualTo(1000);
+        assertThat(discount).isEqualTo(1000);
     }
 
 
     @Test
     @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다.")
     void vip_x(){
+
+        Member member = new Member(2L, "memberVip", Grade.BASIC);
+
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+
+        //then
+
+        assertThat(discount).isEqualTo(0);
 
     }
 
